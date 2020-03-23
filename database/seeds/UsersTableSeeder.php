c697193815a510ce8ba7cce76f122fba957a7a7a
Yuta <y.sugiyama54@gmail.com>
2020-03-23 18:10:25 +0900

    SeederクラスにAdminユーザーを追加

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'password' => bcrypt('admin'),
+            'email' => 'admin@gmail.com',
+            'admin' => 1,
+        ]);
+    }
+}
