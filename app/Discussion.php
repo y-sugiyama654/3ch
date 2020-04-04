2ec25db64c5ec20a335ba5cc852505df54a4fba5
Yuta <y.sugiyama54@gmail.com>
2020-04-04 17:30:26 +0900

    ベストアンサーがついた投稿にクローズラベルを表示

--- app/Discussion.php
+++ app/Discussion.php
@@ -95,4 +95,18 @@ class Discussion extends Model
         }
     }
 
+    public function hasBestAnswer()
+    {
+        $result = false;
+        foreach($this->replies as $reply)
+        {
+            if($reply->discussion_id) {
+                $result = true;
+                break;
+            }
+        }
+
+        return $result;
+    }
+
 }
--- resources/views/partials/discussion-header.blade.php
+++ resources/views/partials/discussion-header.blade.php
@@ -1,19 +1,50 @@
+@section('css')
+    <link href="{{ asset('css/discussion-header.css') }}" rel="stylesheet">
+
+@endsection
 <div class="card-header">
-    <div class="d-flex justify-content-between">
-        <div>
-            <img width="40px" height="40px" style="border-radius: 50%" src="{{ Gravatar::src($discussion->author->email) }}" alt="">
-            <span class="ml-2 mr-2 font-weight-bold">{{ $discussion->author->name }}</span><strong><i class="fas fa-donate"></i> {{ $discussion->author->point }}P</strong>
-        </div>
-        <div>
-            @if (request()->path() == 'discussions')
-                <a href="{{ route('discussions.show', $discussion->slug) }}" class="btn btn-success btn-sm">View</a>
-            @else
-                @if ($discussion->is_being_watched_by_auth_user())
-                    <a href="{{ route('discussions.unwatch', $discussion->id) }}" class="btn btn-success btn-sm">Unwatch</a>
+
+    @if($discussion->hasBestAnswer())
+        {{--  CLOSE  --}}
+        <div class="d-flex justify-content-between">
+            <div class="ribbon18-content">
+                <span class="ribbon18">CLOSED</span>
+            </div>
+            <div>
+                <img width="40px" height="40px" style="border-radius: 50%" src="{{ Gravatar::src($discussion->author->email) }}" alt="">
+                <span class="ml-2 mr-2 font-weight-bold">{{ $discussion->author->name }}</span><strong><i class="fas fa-donate"></i> {{ $discussion->author->point }}P</strong>
+            </div>
+            <div>
+                @if (request()->path() == 'discussions')
+                    <a href="{{ route('discussions.show', $discussion->slug) }}" class="btn btn-success btn-sm mr-5">View</a>
                 @else
-                    <a href="{{ route('discussions.watch', $discussion->id) }}" class="btn btn-success btn-sm">Watch</a>
+                    @if ($discussion->is_being_watched_by_auth_user())
+                        <a href="{{ route('discussions.unwatch', $discussion->id) }}" class="btn btn-success btn-sm mr-5">Unwatch</a>
+                    @else
+                        <a href="{{ route('discussions.watch', $discussion->id) }}" class="btn btn-success btn-sm mr-5">Watch</a>
+                    @endif
                 @endif
-            @endif
+            </div>
         </div>
-    </div>
+    @else
+        {{--  OPEN  --}}
+        <div class="d-flex justify-content-between">
+            <div>
+                <img width="40px" height="40px" style="border-radius: 50%" src="{{ Gravatar::src($discussion->author->email) }}" alt="">
+                <span class="ml-2 mr-2 font-weight-bold">{{ $discussion->author->name }}</span><strong><i class="fas fa-donate"></i> {{ $discussion->author->point }}P</strong>
+            </div>
+            <div>
+                @if (request()->path() == 'discussions')
+                    <a href="{{ route('discussions.show', $discussion->slug) }}" class="btn btn-success btn-sm">View</a>
+                @else
+                    @if ($discussion->is_being_watched_by_auth_user())
+                        <a href="{{ route('discussions.unwatch', $discussion->id) }}" class="btn btn-success btn-sm">Unwatch</a>
+                    @else
+                        <a href="{{ route('discussions.watch', $discussion->id) }}" class="btn btn-success btn-sm">Watch</a>
+                    @endif
+                @endif
+            </div>
+        </div>
+    @endif
+
 </div>
